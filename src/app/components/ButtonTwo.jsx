3394158efa3cd6146c94e1ef5578e672dd951721
24LP3548 24LP3548 <24LP3548@G304-CC0228.local>
2025-01-14 12:47:18 +0800

    second page edited

--- src/app/components/ButtonTwo.jsx
+++ src/app/components/ButtonTwo.jsx
@@ -1,7 +1,44 @@
 import { ChevronRight } from "lucide-react";
 import { ChevronLeft } from "lucide-react";
 
-export const ButtonTwo = ({ label, onClick, goBack }) => {
+export const ButtonTwo = ({
+  setErrorMessage,
+  currentStep,
+  setCurrenStep,
+  userInfo,
+  label,
+}) => {
+  const checkValue = () => {
+    const specialCharacters = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    const checkError = {
+      email: !userInfo.email
+        ? "Email field is empty"
+        : /[^a-zA-Z]/.test(userInfo.email)
+        ? "Email cannot contain special characters or numbers."
+        : "",
+      phone: !userInfo.phone.length ? "Your phone number is wrong" : "",
+      // password: !userInfo.username
+      //   ? "Username field is empty."
+      //   : userInfo.username.length < 6
+      //   ? "Username must be contain 6 or more characters"
+      //   : /^\d+$/.test(userInfo.username)
+      //   ? "Username cannot be only numbers"
+      //   : specialCharacters.test(userInfo.username)
+      //   ? "Username cannot contain special characters"
+      //   : "",
+    };
+
+    setErrorMessage(checkError);
+    if (!Object.values(checkError).some((v) => v))
+      setCurrenStep(currentStep + 1);
+  };
+
+  const goBack = () => {
+    setCurrenStep(currentStep - 1);
+  };
+
   return (
     <div className="w-full overflow-hidden flex justify-center gap-3">
       <button
@@ -14,7 +51,7 @@ export const ButtonTwo = ({ label, onClick, goBack }) => {
         </div>
       </button>
       <button
-        onClick={onClick}
+        onClick={checkValue}
         className="w-3/4 bg-black text-white h-[44px] pt-[10px] pb-[10px] pl-[12px] pr-[12px] rounded-lg"
       >
         <div className="flex justify-center items-center">
